dcf53e015ffccbf9f8e815ea72961ae010509e72
zianksm <mynameiszian@gmail.com>
2025-03-25 18:33:11 +0800

    chore: rename

--- test/TestHook.t.sol
+++ test/TestHook.t.sol
@@ -1,15 +1,58 @@
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.0;
 
-import {TestBase} from "./Base.sol";
+import "./Base.sol";
 
-contract TestHedgeHook is TestBase {
-
+contract TestHook is TestBase {
     function setUp() external {
+        vm.startPrank(DEFAULT_ADDRESS);
         setupHedgeHookTest();
-        initializeProtectedPool();
+        moduleCore.depositLv(defaultCurrencyId, 10 ether, 0, 0);
+    }
 
-        // todo
-        // mimic what this code does
-        seedMoreLiquidity(_key, amount0, amount1);
+    function testHedgeDirectDs() external {
+        addLiquidity(10 ether, 10 ether, "");
+
+        uint256 depositAmount = 10 ether;
+        moduleCore.depositPsm(defaultCurrencyId, depositAmount);
+
+        (, address ds) = moduleCore.swapAsset(defaultCurrencyId, 1);
+        _maxApprove(ds);
+
+        HedgeWithDsParams memory params = buildHedgeWithDsParams(depositAmount);
+
+        hedgehook.hedgeWithDs(params);
+
+        Hedges memory hedge = hedgehook.getHedge(defaultKey, defaultCurrencyId);
+
+        assertEq(hedge.dsBalance, depositAmount);
+    }
+
+    function testHedgeInDirectWithDs() external {
+        uint256 depositAmount = 10 ether;
+        moduleCore.depositPsm(defaultCurrencyId, depositAmount);
+
+        (, address ds) = moduleCore.swapAsset(defaultCurrencyId, 1);
+        _maxApprove(ds);
+
+        HedgeWithDsParams memory params = buildHedgeWithDsParams(depositAmount);
+
+        addLiquidity(10 ether, 10 ether, abi.encode(DEFAULT_ADDRESS, params));
+
+        Hedges memory hedge = hedgehook.getHedge(defaultKey, defaultCurrencyId);
+
+        assertEq(hedge.dsBalance, depositAmount);
+    }
+
+    function testHedgeDirectRa() external {
+        uint256 depositAmount = 10 ether;
+        addLiquidity(10 ether, 10 ether, "");
+
+        HedgeWithRaParams memory params = buildHedgeWithRaParams(depositAmount);
+
+        hedgehook.hedgeWithRa(params);
+
+        Hedges memory hedge = hedgehook.getHedge(defaultKey, defaultCurrencyId);
+
+        assertGe(hedge.dsBalance, depositAmount);
     }
 }
